db07fd3e0492c60bff245fcab541d4f0cbe13971
ddbriggs95 <dowlingbriggs@gmail.com>
2023-04-06 17:17:25 -0500

    Added starter variables and first user prompt for character length

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
-// Assignment code here
-
+//Define variables 
+var characterLength = 8;
+var choiceArray = [];
+var specialCharArray = [];
+var uppCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L'];
+var lowCaseArray = [];
+var numberArray = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +21,27 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generatePassword function to activate button
+function generatePassword(){
+  //first code to prompt user to input password length
+  var passLength = prompt("Enter number of characters between 8 and 128 for password length");
+  console.log("passLength = ", passLength);
+  passLength = parseInt(passLength);
+  var lower = false;
+  var upper = false;
+  var special = false;
+  var num = false;
+  if(passLength >= 8 && passLength <= 128) {
+    lower = confirm("press OK to include lowercase characters in password");
+  }
+  else {
+    alert("The password must be number between 8-128. Please re write.");
+  }
+
+  return "Generated Password will go here"; //return variable for created password here
+
+}
+
+function getPrompts(){
+
+}
